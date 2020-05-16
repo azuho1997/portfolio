f10fe90e359063490c9a76762b25ea40437ad3e8
kazu <tel09038248964@mineo.jp>
2020-05-16 14:01:51 +0900

    update : イメージの削除

--- pages/next.vue
+++ pages/next.vue
@@ -50,11 +50,6 @@
       </div>
   </div>
   <footer>
-    <a class="link" href="https://github.com/ohuzak-1997/portfolio">
-      <span class="fas fa-heart fa-4x" style="color: rgb(45, 119, 119)">
-        <font-awesome-icon :icon="['fab','github']" />
-        </span>
-    </a>
     PORTFOLIO
   </footer>
 </div>
